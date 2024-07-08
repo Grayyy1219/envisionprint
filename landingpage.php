b94a4dad4c6dccd1c482a28ce2c439296554bfc8
Grayyy1219 <lance.musngi@gamil.com>
2024-07-08 22:54:33 +0800

    update

--- landingpage.php
+++ landingpage.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EnVision Print</title>
+    <title>EnVision Print.</title>
     <?php
     include("connect.php");
     include("query.php"); ?>
